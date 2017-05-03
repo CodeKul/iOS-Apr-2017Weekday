ad99297ede0627c015640206a9590f93faa3de33
abhi <abhijeetvc7@gmail.com>
2017-05-03 09:53:06 +0530

    TabBar demo

---
+++ iOSDemos/TabbarDemo/TabbarDemo/FirstViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstViewController.swift
+//  TabbarDemo
+//
+//  Created by Abhi on 03/05/17.
+//  Copyright © 2017 CodeKul. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("FirstViewController :: viewDidLoad")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
