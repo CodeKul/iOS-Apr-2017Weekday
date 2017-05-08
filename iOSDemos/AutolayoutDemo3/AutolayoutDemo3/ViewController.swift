be1ce289ea0b42e3a4c7c3b386bf4ec5307ee660
abhi <abhijeetvc7@gmail.com>
2017-05-08 09:35:01 +0530

    Slider menu demo

---
+++ iOSDemos/AutolayoutDemo3/AutolayoutDemo3/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  AutolayoutDemo3
+//
+//  Created by Abhi on 08/05/17.
+//  Copyright © 2017 CodeKul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var leftSideConstraintsForMenu : NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func menuButtonClicked(_ sender : UIButton) {
+        
+        if leftSideConstraintsForMenu.constant == 0 {
+            leftSideConstraintsForMenu.constant = -200
+        }
+        else {
+            leftSideConstraintsForMenu.constant = 0
+        }
+
+        UIView.animate(withDuration: 2) {
+            
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
