811b4fe515ff7d4054374cb3d258dec9fb2cdb88
varunkudalkar <Codekul@Macs-iMac.local>
2017-05-16 10:14:06 +0530

    SQLiteDemo

---
+++ iOSDemos/SQLiteDemo/SQLiteDemo/SQLiteDemo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SQLiteDemo-Bridging-Header.h
+//  SQLiteDemo
+//
+//  Created by Codekul on 16/05/17.
+//  Copyright © 2017 CodeKul. All rights reserved.
+//
+
+#ifndef SQLiteDemo_Bridging_Header_h
+#define SQLiteDemo_Bridging_Header_h
+
+
+#endif /* SQLiteDemo_Bridging_Header_h */
+
+#import <sqlite3.h>
