d8e62b73666c607a7dc03996bea3ffc708822388
varunkudalkar <Codekul@Macs-iMac.local>
2017-05-22 13:34:31 +0530

    student_app

---
+++ Nachiket/classApp/classApp/addBatchViewController.swift
@@ -0,0 +1,36 @@
+//
+//  addBatchViewController.swift
+//  classApp
+//
+//  Created by Codekul on 22/05/17.
+//  Copyright © 2017 CodeKul. All rights reserved.
+//
+
+import UIKit
+
+class addBatchViewController: UIViewController {
+
+    @IBOutlet weak var txtName: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
