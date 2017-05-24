644cc6c3c611a8ab1815f711c190db3f16d604f3
Varun <varun.kudalkar@gmail.com>
2017-05-24 10:01:28 +0530

    CoreData Demo

--- Priya/CoreDataDemo/CoreDataDemo/ViewController.swift
+++ Priya/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -27,10 +27,10 @@ class ViewController: UIViewController {
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        let stu = NSEntityDescription.entity(forEntityName: "Student", in: context) as! Student
-        stu.rno = txtrno.text
-        stu.name = txtName.text
-        stu.marks = txtMarks.text
+        let stu = NSEntityDescription.insertNewObject(forEntityName: "Student", into: context) as! Student
+        stu.rno = txtrno.text!
+        stu.name = txtName.text!
+        stu.marks = txtMarks.text!
         
         appDelegate.saveContext()
         clearFields()
@@ -41,6 +41,86 @@ class ViewController: UIViewController {
         txtrno.text = ""
         txtName.text = ""
         txtMarks.text = ""
+    }
+    
+    @IBAction func btnShow(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let studentFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        
+        do{
+            let fetchStudent = try context.fetch(studentFetch) as! [Student]
+            
+            for stu in fetchStudent {
+                print("Role No : \(stu.rno!) \n Name : \(stu.name!)\nMarks : \(stu.marks!)")
+            }
+        }catch{
+            fatalError("Failed to fetch students : \(error)")
+        }
+        
+    }
+    
+    @IBAction func btnSearch(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let studentFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        studentFetch.predicate = NSPredicate(format: "rno == %@", txtrno.text!)
+        do{
+            let fetchStudent = try context.fetch(studentFetch) as! [Student]
+            
+            for stu in fetchStudent {
+                txtName.text = stu.name!
+                txtMarks.text = stu.marks!
+            }
+        }catch{
+            fatalError("Failed to fetch students : \(error)")
+        }
+        
+        
+    }
+    
+    
+    @IBAction func btnUpdate(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+
+        let studentFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        studentFetch.predicate = NSPredicate(format: "rno == %@", txtrno.text!)
+        do{
+            let fetchStudent = try context.fetch(studentFetch) as! [Student]
+            
+            fetchStudent[0].name = txtName.text!
+            fetchStudent[0].marks = txtMarks.text!
+            
+        }catch{
+            fatalError("Failed to fetch students : \(error)")
+        }
+        
+        clearFields()
+        
+    }
+    
+    @IBAction func btnDelete(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let studentFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        studentFetch.predicate = NSPredicate(format: "rno == %@", txtrno.text!)
+        
+        do{
+            let fetchStudent = try context.fetch(studentFetch) as! [Student]
+            
+            context.delete(fetchStudent[0])
+            appDelegate.saveContext()
+            
+        }catch{
+            fatalError("Failed to fetch students : \(error)")
+        }
+
+        
+        
     }
     
     
